8e1f2cfe6e0397eabe876a83f9a81d4b10dc7ab6
Eduardo Calvachi <ecalvachi@wccnet.edu>
2017-02-07 14:03:47 -0500

    initial setup

---
+++ js/compare.js
@@ -0,0 +1,14 @@
+
+var objectA =
+{
+    a: "A string",
+    b: true,
+    c: 1234,
+    d: new Date(),
+    e: [3, 4, 5]
+};
+
+var objectB =
+{
+    a: "Another String"
+};
---
+++ js/data.js
@@ -0,0 +1,17 @@
+var objectA =
+{
+    a: "A string",
+    b: true,
+    c: 1234,
+    d: new Date(),
+    e: [3, 4, 5, 'blueberry']
+};
+
+var objectB =
+{
+    a: "Another String",
+    b: false,
+    c: 1234.56,
+    d: new Date(),
+    e: [0, 21, 4, 'banana']
+};

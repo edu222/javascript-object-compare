8eb795c999efb5f63d7cf88c98bfd9b638804956
Eduardo Calvachi <educalvachi@gmail.com>
2017-02-07 14:23:50 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,17 +1,14 @@
 var objectA =
 {
-    a: "A string",
-    b: true,
-    c: 1234,
-    d: new Date(),
-    e: [3, 4, 5, 'blueberry']
+    subjectCode: "MTH",
+    termCode: 201701,
+    course : "101",
+    
 };
 
 var objectB =
 {
-    a: "Another String",
-    b: false,
-    c: 1234.56,
-    d: new Date(),
-    e: [0, 21, 4, 'banana']
+    subjectCode: "ENG",
+    termCode: 201705
+    
 };

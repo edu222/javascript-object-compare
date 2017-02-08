8927e1467f3a41972d4fefb98d8f14843ea71571
Eduardo Calvachi <educalvachi@gmail.com>
2017-02-08 17:37:24 -0500

    Update compare.js
    Refactor

--- js/compare.js
+++ js/compare.js
@@ -1,41 +1,30 @@
-
-function Comparison (target, data){
-    this.target = target;
-    this.data = data;
-    
-    this.compareKeys = function(){
-        var keyMatches = [];
-        for (var targetKeys in this.target){
-            for (var dataKeys in this.data){
-                if (targetKeys === dataKeys){
-                    keyMatches.push(targetKeys);
-                }
+function compare (target, data)
+{    
+    // Find key matches
+    var keyMatches = [];
+    for (var targetKeys in target)
+    {
+        for (var dataKeys in data)
+        {
+            if (targetKeys === dataKeys)
+            {
+                keyMatches.push(targetKeys);
             }
         }
-        return keyMatches;
     }
-    this.compareData = function(){
-        var keyMatches = this.compareKeys();
-        var dataMatches = 0;
-       
-        for (var index in keyMatches){
-            var key = keyMatches[index];
-
-            if(this.target[key] === this.data[key]){
-                dataMatches++;
-            }
+    // Find data matches for keys that match
+    var dataMatches = 0;       
+    for (var index in keyMatches)
+    {
+        var key = keyMatches[index];
+        if(target[key] === data[key])
+        {
+            dataMatches++;
         }
-        return dataMatches;
     }
-    this.compare = function (){      
-        var matches = this.compareData();
-        var fieldsInA = Object.keys(this.target).length;
-        var fieldsInB = Object.keys(this.data).length;
+    // Compute comparison level
+    var fieldsInA = Object.keys(target).length;
+    var fieldsInB = Object.keys(data).length;
 
-        return 2 * matches/(fieldsInA + fieldsInB)
-    }
-    
-    this.search = function(blah){
-        document.write(blah)
-    }
+    return 2 * matches/(fieldsInA + fieldsInB);
 }

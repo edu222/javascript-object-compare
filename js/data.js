6f4905fe2be603b482cd158fad1cfa4072e84cb4
Eduardo Calvachi <ecalvachi@wccnet.edu>
2017-02-08 11:09:12 -0500

    formatting

--- js/data.js
+++ js/data.js
@@ -2,13 +2,13 @@ var objectA =
 {
     subjectCode: "MTH",
     termCode: 201701,
-    course : "101",
-    
+    course : "101"
 };
 
 var objectB =
 {
     subjectCode: "ENG",
-    termCode: 201705
+    termCode: 201705,
+    course: 101
     
 };
